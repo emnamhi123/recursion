046970088bc120fb4f30801d18aaba948d1e7e1d
github_emnamhi123 <emna-mhiri@hotmail.com>
2024-02-26 22:24:28 +0100

    first commit

---
+++ exercice.js
@@ -0,0 +1,17 @@
+function inverseword(str) {
+let reversedWord=str.toLowerCase().split(' ').reverse().join('')
+if(str.toLowerCase()===reversedWord){
+    return true
+}else {return false }
+
+}
+console.log(inverseword('kayak'))
+	// ///////////////////////////////////////////////
+
+function inverse(str){
+    let istr=str.toLowerCase().split(' ').reverse().join('')
+    if(str.toLowerCase()===istr){return true}
+    else{return false}
+
+}
+console.log(inverse('kayak'))
